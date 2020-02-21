ec8b1acf4a73f8c70961466adda3b693f7556019
manchandajayant <maanjayant@gmail.com>
2020-02-21 16:48:48 +0100

    updated Cart buttons

--- src/store/products/productReducer.js
+++ src/store/products/productReducer.js
@@ -29,22 +29,27 @@ export default function productReducer(state = initialState, action) {
 
     case "CART_SUBTRACTED": {
       const productId = action.payload;
+
       const productCart = state.cart.find(p => p.id === productId);
 
       if (!productCart) {
-        const product = state.list.find(p => p.id === productId);
-        return {
-          ...state,
-          cart: [...state.cart, { ...product, quantity: 1 }]
-        };
+        return state;
       } else {
-        const updatedCart = state.cart.map(p =>
-          p.id === productId ? { ...p, quantity: p.quantity - 1 } : p
-        );
-        return {
-          ...state,
-          cart: updatedCart
-        };
+        if (productCart.quantity > 1) {
+          const updatedCart = state.cart.map(p =>
+            p.id === productId ? { ...p, quantity: p.quantity - 1 } : p
+          );
+          return {
+            ...state,
+            cart: updatedCart
+          };
+        } else {
+          const updatedCart = state.cart.filter(p => p.id !== productId);
+          return {
+            ...state,
+            cart: updatedCart
+          };
+        }
       }
     }
 
