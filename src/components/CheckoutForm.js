832ff8edc579e63ce53be256987f47c0330320a0
Mellieejj <Melanieverheul1992@gmail.com>
2020-02-21 19:23:36 +0100

    setup checkout form

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -2,6 +2,31 @@ import React, { Component } from "react";
 
 export default class CheckoutForm extends Component {
   render() {
-    return "hello";
+    return (
+      <div>
+        <form>
+          <label>
+            Name:
+            <input name="name" type="text" />
+          </label>
+          <br />
+          <label>
+            Street:
+            <input name="street" type="text" />
+          </label>
+          <br />
+          <label>
+            House number:
+            <input name="number" type="text" />
+          </label>
+          <br />
+          <label>
+            Postcode:
+            <input name="postcode" type="text" />
+          </label>
+          <br />
+        </form>
+      </div>
+    );
   }
 }
